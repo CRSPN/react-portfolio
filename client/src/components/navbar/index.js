042067263c1c8b2653a5723de4cce9b053b616f5
CRSPN <doeraysme@gmail.com>
2020-11-12 15:58:44 -0700

    updated some html content

--- client/src/components/navbar/index.js
+++ client/src/components/navbar/index.js
@@ -6,7 +6,7 @@ function Navbar (){
         <div className="nav-wrapper container">
           <a id="logo-container" href="#" className="brand-logo">C.Allred</a>
           <ul className="right hide-on-med-and-down">
-            <li><a href="#">Navbar Link</a></li>
+            <li><a href="#">bio</a></li>
             <li><a href="assets/html/resume.html">Resume</a></li>
             <li><a href="assets/html/art.html">Art</a></li>
             <li><a href="#"></a></li>
